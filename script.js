d13914424e092c7b9ad0cb1b65d818d3be37be00
pullant <108009450+pullant@users.noreply.github.com>
2024-02-20 10:43:18 -0300

    Add writing on grid

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let body = document.querySelector("body"); // select body
+let body = document.querySelector("body");
 body.setAttribute("id", "container");
-body.setAttribute("style", "display: flex; flex-direction: column; height: 100vh; margin: 0;");
+body.setAttribute("style", "margin: 30px auto; width: fit-content;");
 
 let rows = [];
 for (let i = 0; i < 16; i++) {
@@ -14,11 +14,16 @@ for (let i = 0; i < 16; i++) {
 
 rows.forEach((row) => {
 	row = document.createElement("div");
-	row.setAttribute("style", "display: flex; flex: 1; border: solid 2px darkred; background-color: coral;");
+	row.setAttribute("style", "display: flex; justify-content: center;");
 	columns.forEach((column) => {
 		column = document.createElement("div");
-		column.setAttribute("style", "flex: 1; border: solid 2px darkblue; background-color: coral;");
+		column.setAttribute("style", "flex: 0 0 auto ; border: solid 1px darkgrey; background-color: lightgrey;");
 		row.appendChild(column);
 	});
 	body.appendChild(row);
+});
+
+body.addEventListener("mouseover", (e) => {
+	targetDiv = e.target;
+	targetDiv.style.backgroundColor = "indigo";
 });
